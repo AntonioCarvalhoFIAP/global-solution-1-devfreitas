b6992a3a32b6e699871e63b813d209d7a1610539
devfreitas <freitassrafa2006@gmail.com>
2026-06-01 14:33:17 -0300

    Refazendo pontos importantes da aplicação, e trocando identidade visual

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components/native';
+import { AuthProvider } from './src/hooks/useAuth';
+import { Routes } from './src/navigation';
+import { theme } from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <StatusBar style="light" backgroundColor={theme.COLORS.PRIMARY} translucent={false} />
+          <Routes />
+        </AuthProvider>
+      </ThemeProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
